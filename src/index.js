a446e244387fcb9222f40fb6f7027750b13818a6
Valentin Prugnaud <valentin@whatdafox.com>
2018-10-26 22:57:08 -0700

    Verify token and display

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 const express = require("express");
-const expressjwt = require("express-jwt");
+const jwt = require('jsonwebtoken');
 const bodyParser = require("body-parser");
+const bearerToken = require('express-bearer-token');
 const app = express();
 
 const PORT = process.env.API_PORT || 8081;
-
-const jwtCheck = expressjwt({
-    secret: "marvel"
-});
+const JSON_SECRET_KEY = 'marvel';
 
 app.use(bodyParser.json());
+app.use(bearerToken());
 
-app.get("/asset", (req, res) => {
+app.get("/assets", (req, res) => {
     res.status(200).send("Everyone can see this.");
 });
 
-app.get("/asset/secret", jwtCheck, (req, res) => {
-    res.status(200).send("You can only see this if you're logged in.");
+app.get("/assets/secret", (req, res) => {
+    jwt.verify(req.token, JSON_SECRET_KEY, (error, decoded) => {
+        if(error) {
+            res.status(401).send('You are not who you think you are!');
+            return false;
+        }
+
+        res.status(200).send("You can only see this because you are " + decoded.username);
+    });
 });
 
 app.get("*", (req, res) => {
